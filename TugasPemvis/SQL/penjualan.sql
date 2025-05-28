54684f63b8ea2245939989095ac58cb70b23e895
crylsywl <crylsyawal123@gmail,com>
2025-05-28 09:18:43 +0700

    NOTA dan LOGIN

--- TugasPemvis/SQL/penjualan.sql
+++ TugasPemvis/SQL/penjualan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 29 Apr 2025 pada 12.00
+-- Waktu pembuatan: 28 Bulan Mei 2025 pada 04.14
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -51,6 +51,30 @@ INSERT INTO `barang` (`KODE BARANG`, `NAMA BARANG`, `JENIS`, `HARGA BELI`, `HARG
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `isi`
+--
+
+CREATE TABLE `isi` (
+  `idnota` varchar(11) NOT NULL,
+  `kdbrg` int(11) NOT NULL,
+  `hargabeli` int(11) NOT NULL,
+  `hargajual` int(11) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `isi`
+--
+
+INSERT INTO `isi` (`idnota`, `kdbrg`, `hargabeli`, `hargajual`, `qty`) VALUES
+('IN0001', 1, 234, 34324, 2),
+('IN0002', 1, 10000, 12000, 1),
+('IN0002', 2, 15000, 18000, 2),
+('IN0002', 3, 3000, 5000, 3);
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `kasir`
 --
 
@@ -71,7 +95,29 @@ CREATE TABLE `kasir` (
 INSERT INTO `kasir` (`ID KASIR`, `NAMA KASIR`, `JENIS KELAMIN`, `TELEPON`, `ALAMAT`, `USERNAME`, `PASSWORD`) VALUES
 (1, 'Cryl', 'Laki-Laki', '625964570263', 'depok', 'crylsywl', 'user123'),
 (2, 'cryl', 'Laki-Laki', '0909777', 'depok', 'c11rylsywl', 'user123'),
-(3, 'roy', 'Laki-Laki', '432345345', 'depok', 'roy123', '240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9');
+(3, 'roy', 'Laki-Laki', '432345345', 'depok', 'roy123', '240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9'),
+(5, 'adit', 'Laki-Laki', '085212332', 'jakarta', 'akabane', '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `nota`
+--
+
+CREATE TABLE `nota` (
+  `idnota` varchar(6) NOT NULL,
+  `tglnota` date NOT NULL,
+  `idplgn` int(6) NOT NULL,
+  `idkasir` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `nota`
+--
+
+INSERT INTO `nota` (`idnota`, `tglnota`, `idplgn`, `idkasir`) VALUES
+('IN0001', '2026-11-21', 324, 'ID KASIR'),
+('IN0002', '2025-05-26', 2, 'ID KASIR');
 
 -- --------------------------------------------------------
 
@@ -111,6 +157,12 @@ ALTER TABLE `barang`
 ALTER TABLE `kasir`
   ADD PRIMARY KEY (`ID KASIR`),
   ADD UNIQUE KEY `USERNAME` (`USERNAME`);
+
+--
+-- Indeks untuk tabel `nota`
+--
+ALTER TABLE `nota`
+  ADD PRIMARY KEY (`idnota`);
 
 --
 -- Indeks untuk tabel `pelanggan`
